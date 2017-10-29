8274cc4e1014c43eb1c5967742a45bdcf1c793a6
nicholasnadel <nicholasnadel@gmail.com>
2017-10-28 23:44:22 -0700

    add articles path & model

--- workspace/node-mongo/app.js
+++ workspace/node-mongo/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -25,6 +26,14 @@ let Article = require('./models/article');
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'pug');
 
+  // Body Parser Middlware
+  app.use(bodyParser.urlencoded({ extended: false }))
+
+  // parse application/json
+  app.use(bodyParser.json())
+
+
+
   // Home Route
   app.get('/', function(req, res){
     Article.find({}, function(err, articles){
@@ -43,6 +52,24 @@ let Article = require('./models/article');
 app.get('/articles/add', function(req, res){
   res.render('add_article', {
     title: 'Add Article'
+  });
+});
+
+
+// Add Submit POST Route
+app.post('/articles/add', function(req, res){
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
